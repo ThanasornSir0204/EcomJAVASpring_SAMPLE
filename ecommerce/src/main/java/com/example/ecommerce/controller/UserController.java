74874213008e32dab42c3a0479dfb0dbebf13772
Thanasorn Sirirattanapong <143977354+ThanasornSir0204@users.noreply.github.com>
2024-05-04 23:58:54 +0700

    fix bug

--- ecommerce/src/main/java/com/example/ecommerce/controller/UserController.java
+++ ecommerce/src/main/java/com/example/ecommerce/controller/UserController.java
@@ -18,7 +18,6 @@ public class UserController {
 
     private final UserRepository userRepository;
 
-    @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
